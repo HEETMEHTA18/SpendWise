b6fe372bd1b3d2973c386df5d335d412e6527a29
rahull0328 <rmmehta2004@gmail.com>
2025-04-12 15:15:21 +0530

    completed login page UI and added signUp page

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+import Image from 'next/image'
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 function Header() {
   return (
     <div className='p-5 flex justify-between items-center border shadow-sm'>
-      <Image src={'./logo.svg'} 
-       alt='logo'
-       width={50}
-       height={50}
-      />
-      <Button>Get Started</Button>
+      {/* Logo + Title section */}
+      <div className='flex items-center space-x-3'>
+        <Image 
+          src={'./logo.svg'} 
+          alt='logo'
+          width={50}
+          height={50}
+        />
+        <span className='text-orange-500 text-xl font-bold'>SpendWise</span>
+      </div>
+
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
     </div>
   )
 }
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -16,12 +18,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${outfit.variable} ${outfit1.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${outfit.variable} ${outfit1.variable} antialiased`}>
+          
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
