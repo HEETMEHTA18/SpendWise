27e784febec2afdf4df23ed4cc7d83dec476ea75
rahull0328 <rmmehta2004@gmail.com>
2025-04-15 13:50:44 +0530

    completed database integrations

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,9 +1,14 @@
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+
+// Load environment variables from .env.local
+dotenv.config({ path: resolve(__dirname, '.env.local') });
+
 export default {
-    dialect:'postgresql',
-    schema:'./utils/schema.jsx',
-    out:'./drizzle',
-    dbCredentials:{
-        url: process.env.NEXT_PUBLIC_DATABASE_URL,
-        connectionStrings:process.env.NEXT_PUBLIC_DATABASE_URL,
-    }
-}
+  dialect: 'postgresql',
+  schema: './utils/schema.jsx',
+  out: './drizzle',
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+  },
+};
