a454f617141f36081e853c79d29cb1372f3d67c3
rahull0328 <rmmehta2004@gmail.com>
2025-04-21 17:33:17 +0530

    started integration OPENAI

--- utils/getFinancialAdvice.jsx
+++ utils/getFinancialAdvice.jsx
@@ -2,23 +2,37 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
     apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-    dangerouslyAllowBrowser
+    dangerouslyAllowBrowser: true
 })
 
-const getFinancialAdvice = async (totalBudget, totalIncome, totalSpent) => {
+const getFinancialAdvice = async (totalBudget, totalIncome, totalSpend) => {
     try {
         const userPrompt = `
         Based on the following financial data:
         - Total Budget: ${totalBudget} INR
-        - Expenses: ${totalSpent} INR
+        - Expenses: ${totalSpend} INR
         - Incomes: ${totalIncome} INR
         provide detailed financial advice in 2 sentences to help the user manage their finances.
         `
 
         const chatCompletion = await openai.chat.completions.create({
             model: 'gpt-4o',
-        })
-    } catch {
-
+            messages: [{ 
+                role: 'user', 
+                content: userPrompt
+            }],            
+        });
+        
+        const advice = chatCompletion.choices[0].message.content
+        
+        return advice;
+    } catch(error) {
+        return (
+           <div>
+                {error.message}
+           </div>
+        );
     }
 }
+
+export default getFinancialAdvice;
